19f55953c0bdb7ce9a69be811c1b8461c1fba417
1552284258 <1552284258@qq.com>
2019-10-22 12:12:25 +0800

    a

--- js/wx-index.js
+++ js/wx-index.js
@@ -32,35 +32,5 @@
     sj()
 
 
-    function DJ() {
 
-        var btn1 = document.querySelector('.btn_one_li'),
-            btn2 = document.querySelector('.btn_two_li'),
-            tc_1 = document.querySelector('.tc_1'),
-            tc_2 = document.querySelectorAll('.tc_2')
-        btn1.onclick = function () {
-            tc_1.style.display='inline-block'
-
-            tc_2.style.display='none'
-        }
-        btn2.onclick = function () {
-            tc_1.style.display='none'
-            tc_2.style.display='inline-block'
-
-        }
-    }
-    DJ()
 })()
-
-    // for(let i=0;i<tabs.length;i++){
-    //     tabs[i].onclick = function(){
-    //         //给当前元素添加类名，兄弟移除current类型
-    //         //先清除所有的current，再给当前元素添加
-    //         for(let j=0;j<tabs.length;j++){
-    //             tabs[j].classList.remove('current')
-    //             bodys[j].classList.remove('current')
-    //         }
-    //         this.classList.add('current')
-    //         bodys[i].classList.add('current')
-    //     }
-    // } 
