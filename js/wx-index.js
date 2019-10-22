691da69601148cf3386a9810490eb6dc70049c2c
1552284258 <1552284258@qq.com>
2019-10-22 09:27:23 +0800

    a

--- js/wx-index.js
+++ js/wx-index.js
@@ -32,89 +32,35 @@
     sj()
 
 
-    // function mode1() {
-    //     let n = 0;
-    //     let $matchBox = null,
-    //         $leftBtn = null,
-    //         $mode1 = null,
-    //         $rightBtn = null,
-    //         $Top = null,
-    //         $Atops = null,
-    //         $Btop = null;
+    function DJ() {
 
-    //     function initEle() {
-    //         $mode1 = $('.mode1')
-    //         $matchBox = $mode1.find('.matchBox')
-    //         $leftBtn = $mode1.find('.btn_left')
-    //         $rightBtn = $mode1.find('.btn_right')
-    //         $Top = $mode1.find('Top')
-    //         $Atops = $mode1.find('Atop')
-    //         $Btop = $mode1.find('Btop')
+        var btn1 = document.querySelector('.btn_one_li'),
+            btn2 = document.querySelector('.btn_two_li'),
+            tc_1 = document.querySelector('.tc_1'),
+            tc_2 = document.querySelectorAll('.tc_2')
+        btn1.onclick = function () {
+            tc_1.style.display='inline-block'
 
-    //     }
-        
-    //     function getData() {
-    //         $.ajax({
-    //             url: '../js/wx-data.json',
-    //             success: function (data) {
-    //                 initEle()
-    //                 render(data)
+            tc_2.style.display='none'
+        }
+        btn2.onclick = function () {
+            tc_1.style.display='none'
+            tc_2.style.display='inline-block'
 
-
-    //                 console.log(data)
-    //             },
-    //             error: function () {
-    //                 console.log('失败')
-    //             }
-    //         })
-    //     }
-    //     getData()
-    //     function render(data) {
-    //         let str = ''
-    //         data.forEach(item => {
-    //             str += ` <li class="Atop">
-    //             <p class="Btop">
-    //                 <div class="Ctop"></div>
-    //                 <span class="Dspan1">${item.start}</span>
-    //                 <span class="Dspan2">${item.time}</span>
-    //                 <span class="Dspan3">${item.day}</span>
-    //             </p>
-    //             <div class="Bleft">
-    //                 <span>
-    //                     <a href="">
-    //                         <img src="${item.img}" alt="">
-    //                     </a>
-    //                 </span>
-    //                 <a>C9</a>
-    //             </div>
-    //             <div class="Bcenter">
-    //                 <a class="zero">0</a>
-    //                 <span class="mh">:</span>
-    //                 <a class="one">1</a>
-    //             </div>
-    //             <div class="Bright">
-    //                 <span>
-    //                     <a href="">
-    //                         <img src="${item.img2}" alt="">
-    //                     </a>
-    //                 </span>
-    //                 <a>C9</a>
-    //             </div>
-    //             <p class="Bdown">
-    //                 <a class="Bdata">数据</a>
-    //                 <a class="Bvideo">视频</a>
-    //             </p>
-    //             <p class="Bbottom">
-    //                 <span>2019全球总决赛</span>
-    //                 <a>小组赛 第六天 BO1</a>
-    //             </p>
-    //         </li>`
-    //         })
-    //         $Atops.html(str)
-    //     }
-
-    // }
-    // mode1()
+        }
+    }
+    DJ()
 })()
 
-
+    // for(let i=0;i<tabs.length;i++){
+    //     tabs[i].onclick = function(){
+    //         //给当前元素添加类名，兄弟移除current类型
+    //         //先清除所有的current，再给当前元素添加
+    //         for(let j=0;j<tabs.length;j++){
+    //             tabs[j].classList.remove('current')
+    //             bodys[j].classList.remove('current')
+    //         }
+    //         this.classList.add('current')
+    //         bodys[i].classList.add('current')
+    //     }
+    // } 
