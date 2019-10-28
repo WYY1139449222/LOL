8df117eb25b60556597badb0798f00997af8e568
Fyh0706 <54092244+Fyh0706@users.noreply.github.com>
2019-10-28 21:32:05 +0800

    ''10-28'

--- js/cat.js
+++ js/cat.js
@@ -130,6 +130,7 @@ playerMechanism();
 let b = 0;
 let c = 0;
 let n = 0;
+
 function playBanner() {
     //点击滑动轮播图
     let $playIns = $('.page3 .playIn');
@@ -164,7 +165,7 @@ function playBanner() {
     })
     $players.on('mouseup', function (e) {
         let h = e.pageX - this.sx;
-     
+
         b = parseFloat($players.eq(0).css("left"));
         if (h > 0) {
             n++;
@@ -205,24 +206,29 @@ function playBanner() {
 }
 playBanner();
 
+function getData(url, fn) {
 
-//获取游戏资料中的头像文字描述
-function getData() {
+
     let xhr = new XMLHttpRequest();
-    xhr.open('get', '../json/profession.json', true);
+    xhr.open('get', url, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && /200|304/.test(xhr.status)) {
             let data = JSON.parse(xhr.response);
-            render(data);
+            console.log(data);
+
+            fn(data)
+
         }
     }
     xhr.send();
 
 }
-getData();
-
+//头像文字渲染
 function render(data) {
-    let professionClass = document.querySelector('.professionClass')
+    let $tips = $('#tips');
+    let ary = [' 法师从攻击中获得的法力值高于正常水平。部署多个法师可以使你的队伍获得法术强度，随着法师数量的增加，获得的法术强度也会提升。', '刺客在作战开始时会潜伏穿越战场，前往其初始位置的对面。部署多个刺客英雄可以增加他们的暴击伤害，随着刺客数量的增加，暴击伤害也会提升。', '部署多个剑士英雄可以使他们的攻击有一定几率进行多次额外攻击，随着剑士英雄数量的增加，这个几率也会提升。', '部署多个斗士英雄可以使他们获得额外的生命值，随着斗士数量的增加，额外生命值也会提升。', '元素使从攻击中获得的法力值高于正常水平。<br>	部署足够多的元素使会在作战开始时召唤出一个元素为你作战。', '部署全部两个护卫可以使其自身以及周围相邻的友军在开始作战时获得护甲', '部署多个枪手可以使他们的攻击有一定几率命中额外的目标，随着枪手数量的增加，这个几率也会提升', '部署多个骑士可以使他们格挡即将遭受的攻击伤害，随着骑士数量的增加，格挡的伤害也会提升。', '部署多个游侠可以使他们有一定几率获得攻击速度激增的效果，随着游侠数量的增加，这个几率也会提升。'];
+    let professionClass = document.querySelector('.professionClass');
+
     let str = '';
     data.forEach(item => {
         str += `
@@ -238,5 +244,104 @@ function render(data) {
             `
     });
     professionClass.innerHTML = str;
+    let $lis = $('.professionClass li'); //更新lis 实现点击变化的效果
+    // console.log($lis);
+    $lis.on('click', function () {
+        let n = $(this).index();
+        $tips.html(ary[n]);
+    });
 
+
+}
+//获取游戏资料中的头像文字描述
+getData('../json/profession.json', render);
+//获取相关英雄渲染
+
+TheHero();
+
+function TheHero() {
+    getData('../json/theHero.json', renderTheHero);
+    let $heroList = $('#hero-list');
+    let $lis = $heroList.find('li');
+    let $cont5Bg = $('.relevant .cont5-bg');
+    let $cont6Bg = $('.relevant .cont6-bg');
+
+    function renderTheHero(data) {
+        // console.log($cont5Bg);
+        let str = '',
+            descStr = '';
+        data.forEach(item => {
+            let {
+                img,
+                peopleName,
+                race,
+                job,
+                price,
+                skilImg,
+                skillName,
+                skillInfo
+            } = item;
+            str += `
+               <li>
+               <img src="${img}" alt="">
+               <div class="after"></div>
+           </li>
+           `;
+            descStr += `    <div class="cont6-bg">
+                             <div class="video-wrap">
+                                <img src="//game.gtimg.cn/images/lol/act/a20190702loltftwf/video-bg.jpg" alt="">
+                                <i class="icon-play"></i>
+                                <div class="aftervideo"></div>
+                            </div>
+                            <div class="text-wrap">
+                                <p>
+                                    <span id='peopleName'>${peopleName}</span>
+                                    <span id='race'>种族：${race}</span>
+                                    <span id='job'>职业：${job}</span>
+                                    <span id="price">价格：${price}</span>
+                                </p>
+                                <div class="textDesc">
+                                    <img id='skillImg' src="${skilImg}" alt="">
+                                    <span id='skillName'> ${skillName}</span>
+                                    <p id='skillInfo'>${skillInfo}</p>
+                                </div>
+                                </div>
+                                </div>  `;
+        })
+        $heroList.html(str);
+        $cont5Bg.html(descStr);
+        $cont6Bg = $('.relevant .cont6-bg');
+        $lis = $heroList.find('li');
+        $cont6Bg.eq(0).show().siblings().hide();
+        
+        $lis.on('click',function(){
+            let n=$(this).index();
+            $(this).addClass('on').siblings().removeClass('on');
+            $cont6Bg.eq(n).show().siblings().hide();
+            
+        })
+      
+
+
+    }
+    //获取相关英雄
+
+
+    //点击相关英雄头衔进行切换英雄描述
+    //    $heroList
+    // let tabTheHeroN = 0;
+      
+   
+
+}
+
+//点击种族的时候切换
+search();
+function search(){
+    let $spans=$(".searchMain span");
+  $spans.on('click',function(){
+      
+      $(this).addClass('onselect').siblings().removeClass('onselect');
+
+  })
 }
